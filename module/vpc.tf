654ff32d883cab46e2c4bd8a579ba52d9ef556be
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-23 17:39:20 +0530

    Update vpc.tf

--- module/vpc.tf
+++ module/vpc.tf
@@ -2,18 +2,7 @@ locals {
   cluster-name = var.cluster-name
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.cidr-block
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
-  tags = {
-    Name = var.vpc-name
-    Env  = var.env
-
-  }
-}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
