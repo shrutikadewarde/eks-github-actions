64bba97c191ceda343adfbbe4645689e38610c3b
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-25 13:20:13 +0530

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,70 +1,134 @@
-variable "aws_region" {}
-variable "env" {
- type = string
+variable "aws_region" {
+  type = string
 }
-variable "cluster_name" {}
-variable "vpc_cidr_block" {}
-variable "vpc_name" {}
-variable "igw_name" {}
 
-variable "pub_subnet_count" {}
+variable "env" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "igw_name" {
+  type = string
+}
+
+variable "pub_subnet_count" {
+  type = number
+}
+
 variable "pub_cidr_block" {
   type = list(string)
-}
-variable "eip_name" {
-  type = string
 }
 
 variable "pub_availability_zone" {
   type = list(string)
 }
-variable "pub_sub_name" {}
 
-variable "pri_subnet_count" {}
+variable "pub_sub_name" {
+  type = string
+}
+
+variable "pri_subnet_count" {
+  type = number
+}
+
 variable "pri_cidr_block" {
   type = list(string)
 }
+
 variable "pri_availability_zone" {
   type = list(string)
 }
-variable "pri_sub_name" {}
 
-variable "public_rt_name" {}
-variable "private_rt_name" {}
+variable "pri_sub_name" {
+  type = string
+}
+
+variable "public_rt_name" {
+  type = string
+}
+
+variable "private_rt_name" {
+  type = string
+}
+
 variable "eip_name" {
   type = string
 }
-variable "ngw_name" {}
-variable "eks_sg" {}
 
-# EKS
+variable "ngw_name" {
+  type = string
+}
+
+variable "eks_sg" {
+  type = string
+}
+
+# EKS variables
 variable "is_eks_cluster_enabled" {
   type = bool
 }
-variable "cluster_version" {}
-variable "endpoint_private_access" {}
-variable "endpoint_public_access" {}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "endpoint_private_access" {
+  type = bool
+}
+
+variable "endpoint_public_access" {
+  type = bool
+}
 
 variable "ondemand_instance_types" {
+  type    = list(string)
   default = ["t3a.medium"]
 }
 
-variable "spot_instance_types" {}
-variable "desired_capacity_on_demand" {
- type = number
+variable "spot_instance_types" {
+  type = list(string)
 }
-variable "min_capacity_on_demand" {}
-variable "max_capacity_on_demand" {}
 
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
+variable "desired_capacity_on_demand" {
+  type = number
+}
+
+variable "min_capacity_on_demand" {
+  type = number
+}
+
+variable "max_capacity_on_demand" {
+  type = number
+}
+
+variable "desired_capacity_spot" {
+  type = number
+}
+
+variable "min_capacity_spot" {
+  type = number
+}
+
+variable "max_capacity_spot" {
+  type = number
+}
 
 variable "addons" {
   type = list(object({
     name    = string
     version = string
-    # Add more fields if needed (e.g. version, config, etc.)
   }))
 }
+
  
