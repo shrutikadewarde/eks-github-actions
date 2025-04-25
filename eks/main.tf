55bca73fa6203a8aed88bc9ba9147ff391c76cdd
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-25 10:34:11 +0530

    Changed to snake-case

--- eks/main.tf
+++ eks/main.tf
@@ -1,50 +1,44 @@
-provider "aws" {
-  region = var.aws_region
+locals {
+  org = "medium"
+  env = var.env
 }
 
-module "EKS" {
+module "eks" {
   source = "../module"
 
-  # General settings
-  aws_region               = var.aws_region
-  env                      = var.env
-  cluster_name             = var.cluster_name
-  cluster_version          = "1.27"
-  is_eks_cluster_enabled   = true
+  env                   = var.env
+  cluster_name          = "${local.env}-${local.org}-${var.cluster-name}"
+  cidr_block            = var.vpc_cidr_block
+  vpc_name              = "${local.env}-${local.org}-${var.vpc-name}"
+  igw_name              = "${local.env}-${local.org}-${var.igw-name}"
+  pub_subnet_count      = var.pub_subnet_count
+  pub_cidr_block        = var.pub_cidr_block
+  pub_availability_zone = var.pub_availability-zone
+  pub_sub_name          = "${local.env}-${local.org}-${var.pub-sub-name}"
+  pri_subnet_count      = var.pri_subnet_count
+  pri_cidr_block        = var.pri_cidr_block
+  pri_availability_zone = var.pri_availability_zone
+  pri_sub_name          = "${local.env}-${local.org}-${var.pri-sub-name}"
+  public_rt-name        = "${local.env}-${local.org}-${var.public-rt-name}"
+  private_rt_name       = "${local.env}-${local.org}-${var.private-rt-name}"
+  eip_name              = "${local.env}-${local.org}-${var.eip-name}"
+  ngw_name              = "${local.env}-${local.org}-${var.ngw-name}"
+  eks_sg                = var.eks_sg
 
-  # Networking
-  vpc_cidr_block           = var.vpc_cidr_block
-  vpc_name                 = var.vpc_name
-  vpc_id                   = var.vpc_id
-  igw_name                 = var.igw_name
-  pub_subnet_count         = var.pub_subnet_count
-  pub_cidr_block           = var.pub_cidr_block
-  pub_availability_zone    = var.pub_availability_zone
-  pub_sub_name             = var.pub_sub_name
-  pri_subnet_count         = var.pri_subnet_count
-  pri_cidr_block           = var.pri_cidr_block
-  pri_availability_zone    = var.pri_availability_zone
-  pri_sub_name             = var.pri_sub_name
-  public_rt_name           = var.public_rt_name
-  private_rt_name          = var.private_rt_name
-  eip_name                 = var.eip_name
-  ngw_name                 = var.ngw_name
-  eks_sg                   = var.eks_sg
+  is_eks_role_enabled           = true
+  is_eks_nodegroup_role_enabled = true
+  ondemand_instance_types       = var.ondemand_instance_types
+  spot_instance_types           = var.spot_instance_types
+  desired_capacity_on_demand    = var.desired_capacity_on_demand
+  min_capacity_on_demand        = var.min_capacity_on_demand
+  max_capacity_on_demand        = var.max_capacity_on_demand
+  desired_capacity_spot         = var.desired_capacity_spot
+  min_capacity_spot             = var.min_capacity_spot
+  max_capacity_spot             = var.max_capacity_spot
+  is-eks-cluster-enabled        = var.is-eks_cluster_enabled
+  cluster-version               = var.cluster_version
+  endpoint-private-access       = var.endpoint_private_access
+  endpoint-public-access        = var.endpoint_public_access
 
-  # Cluster access
-  endpoint_private_access  = var.endpoint_private_access
-  endpoint_public_access   = var.endpoint_public_access
-
-  # Node groups
-  ondemand_instance_types    = var.ondemand_instance_types
-  spot_instance_types        = var.spot_instance_types
-  desired_capacity_on_demand = var.desired_capacity_on_demand
-  min_capacity_on_demand     = var.min_capacity_on_demand
-  max_capacity_on_demand     = var.max_capacity_on_demand
-  desired_capacity_spot      = var.desired_capacity_spot
-  min_capacity_spot          = var.min_capacity_spot
-  max_capacity_spot          = var.max_capacity_spot
-
-  # EKS Add-ons
   addons = var.addons
 }
