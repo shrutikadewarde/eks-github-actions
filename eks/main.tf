0d7a7c53d89a6fa918fbc2204793803c453e2176
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-25 10:36:27 +0530

    no change in snake-case main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -7,23 +7,23 @@ module "eks" {
   source = "../module"
 
   env                   = var.env
-  cluster_name          = "${local.env}-${local.org}-${var.cluster-name}"
-  cidr_block            = var.vpc_cidr_block
-  vpc_name              = "${local.env}-${local.org}-${var.vpc-name}"
-  igw_name              = "${local.env}-${local.org}-${var.igw-name}"
-  pub_subnet_count      = var.pub_subnet_count
-  pub_cidr_block        = var.pub_cidr_block
-  pub_availability_zone = var.pub_availability-zone
-  pub_sub_name          = "${local.env}-${local.org}-${var.pub-sub-name}"
-  pri_subnet_count      = var.pri_subnet_count
-  pri_cidr_block        = var.pri_cidr_block
-  pri_availability_zone = var.pri_availability_zone
-  pri_sub_name          = "${local.env}-${local.org}-${var.pri-sub-name}"
-  public_rt-name        = "${local.env}-${local.org}-${var.public-rt-name}"
-  private_rt_name       = "${local.env}-${local.org}-${var.private-rt-name}"
-  eip_name              = "${local.env}-${local.org}-${var.eip-name}"
-  ngw_name              = "${local.env}-${local.org}-${var.ngw-name}"
-  eks_sg                = var.eks_sg
+  cluster-name          = "${local.env}-${local.org}-${var.cluster-name}"
+  cidr-block            = var.vpc-cidr-block
+  vpc-name              = "${local.env}-${local.org}-${var.vpc-name}"
+  igw-name              = "${local.env}-${local.org}-${var.igw-name}"
+  pub-subnet-count      = var.pub-subnet-count
+  pub-cidr-block        = var.pub-cidr-block
+  pub-availability-zone = var.pub-availability-zone
+  pub-sub-name          = "${local.env}-${local.org}-${var.pub-sub-name}"
+  pri-subnet-count      = var.pri-subnet-count
+  pri-cidr-block        = var.pri-cidr-block
+  pri-availability-zone = var.pri-availability-zone
+  pri-sub-name          = "${local.env}-${local.org}-${var.pri-sub-name}"
+  public-rt-name        = "${local.env}-${local.org}-${var.public-rt-name}"
+  private-rt-name       = "${local.env}-${local.org}-${var.private-rt-name}"
+  eip-name              = "${local.env}-${local.org}-${var.eip-name}"
+  ngw-name              = "${local.env}-${local.org}-${var.ngw-name}"
+  eks-sg                = var.eks-sg
 
   is_eks_role_enabled           = true
   is_eks_nodegroup_role_enabled = true
@@ -35,10 +35,10 @@ module "eks" {
   desired_capacity_spot         = var.desired_capacity_spot
   min_capacity_spot             = var.min_capacity_spot
   max_capacity_spot             = var.max_capacity_spot
-  is-eks-cluster-enabled        = var.is-eks_cluster_enabled
-  cluster-version               = var.cluster_version
-  endpoint-private-access       = var.endpoint_private_access
-  endpoint-public-access        = var.endpoint_public_access
+  is-eks-cluster-enabled        = var.is-eks-cluster-enabled
+  cluster-version               = var.cluster-version
+  endpoint-private-access       = var.endpoint-private-access
+  endpoint-public-access        = var.endpoint-public-access
 
   addons = var.addons
 }
