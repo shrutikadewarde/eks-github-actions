a91466be5f372af46b805286aa93d9ca0f95c62f
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-25 12:29:10 +0530

    Update gather.tf

--- module/gather.tf
+++ module/gather.tf
@@ -1,4 +1,4 @@
-data "tls_certificate" "eks-certificate" {
+data "tls_certificate" "eks_certificate" {
   url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
 }
 
@@ -9,13 +9,14 @@ data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks-oidc.url, "https://", "")}:sub"
+      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:default:aws-test"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.eks-oidc.arn]
+      identifiers = [aws_iam_openid_connect_provider.eks_oidc.arn]
       type        = "Federated"
     }
   }
 }
+
